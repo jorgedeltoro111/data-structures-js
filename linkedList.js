5a42f61776acded957553c7f16704dae7c657ae8
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-11-17 14:51:51 -0600

    Funcion insert

--- linkedList.js
+++ linkedList.js
@@ -14,7 +14,7 @@ class List{
         this.length = 1;
     }
 
-    append(value){
+    append(value){ 
         let nodo = new Node(value); 
         this.tail.pointer = nodo;
         this.tail= nodo;
@@ -24,14 +24,40 @@ class List{
     getList(){
         let nodo = new Node();
         nodo = this.head;
-        while(nodo.pointer !== null){
+        while(nodo !== null){
             console.log(nodo.value);
             nodo = nodo.pointer; 
         }
+    }
+
+    prepend(value){
+        const newNodo = new Node(value);
+        newNodo.pointer = this.head;
+        this.head = newNodo;
+        this.length++;
+    }
+
+    insert(value, index){
+        const newNode = new Node(value);
+        let aux = this.head;
+        while(aux !== null){
+            if(aux.value === index){
+                newNode.pointer = aux.pointer;
+                aux.pointer = newNode;
+                this.length++;
+                if(aux.value === this.tail.value){
+                    this.tail = newNode;
+                }    
+                return;
+            }
+            aux = aux.pointer;
+        }
+        console.log('Indice no encontrado');
     }
 }
 
 let list = new List(5);
 list.append(10);
 list.append(15);
+list.append(25);
 list.getList();
