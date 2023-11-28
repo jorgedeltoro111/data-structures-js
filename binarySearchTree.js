4bb95dc3119e15210014c25dc631b4b03daef3da
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-11-28 00:22:28 -0600

    Search finished

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -38,4 +38,45 @@ class BinarySearchTree {
             }
         }
     }
+
+    printTree(node = this.root, level = 0, prefix = "Root: ") {
+        if (node !== null) {
+            console.log("  ".repeat(level) + prefix + node.value);
+            this.printTree(node.left, level + 1, "L: ");
+            this.printTree(node.right, level + 1, "R: ");
+        }
+    }
+
+    display() {
+        this.printTree();
+    }
+
+    search(value){
+        let aux = this.root;
+        if(this.length > 0){
+            while(aux !== null){
+                if(aux.value === value){
+                    console.log('The value exist!');
+                    return;
+                }else if(value > aux.value){
+                    aux = aux.right;
+                }else{
+                    aux = aux.left;
+                }
+            }
+            console.log('The value does not exist!');
+        }else{
+            console.log('The binary tree is empty!');
+        }
+    }
+
+    
 }
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(12);
+tree.insert(9);
+tree.insert(11);
+tree.insert(14);
+tree.search(8);
