7d697b11a5d838949a171f9787dead0558814f25
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-12-03 00:23:30 -0600

    Graph created

--- graph.js
+++ graph.js
@@ -13,6 +13,23 @@ class Graph {
         this.adjacentList[node1].push(node2);
         this.adjacentList[node2].push(node1);
     }
+    get(){
+        console.log(this.adjacentList);
+    }
 }
 
 const graph = new Graph();
+graph.addVertex("1");
+graph.addVertex("3");
+graph.addVertex("4");
+graph.addVertex("5");
+graph.addVertex("6");
+graph.addVertex("8");
+graph.addEdge("8", "4");
+graph.addEdge("4", "1");
+graph.addEdge("4", "5");
+graph.addEdge("5", "3");
+graph.addEdge("1", "3");
+graph.addEdge("1", "6");
+graph.addEdge("3", "6");
+graph.get();
